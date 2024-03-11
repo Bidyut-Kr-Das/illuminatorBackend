505939fc2a1445443f92e5bd336c0acf12df3477
Bidyut Kr Das <bkdas2017.bd@gmail.com>
2024-03-11 22:35:24 +0530

    jwt added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,14 @@
+import jwt from "jsonwebtoken";
+
 import catchAsyncError from "#utils/catchAsyncError.js";
 
 import User from "#models/user.model.js";
+
+const generateAccessToken = async id => {
+  return await jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
 
 export const getUserInfo = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
@@ -13,6 +21,7 @@ export const getUserInfo = catchAsyncError(async (req, res, next) => {
 });
 
 export const createUser = catchAsyncError(async (req, res, next) => {
+  console.log(req.body);
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -21,9 +30,11 @@ export const createUser = catchAsyncError(async (req, res, next) => {
     confPassword: req.body.confPassword,
     gender: req.body.gender
   });
+  const token = await generateAccessToken(newUser._id);
   res.status(201).json({
     status: "success",
     message: "User created successfully",
+    token,
     data: {
       user: newUser
     }
--- src/cors/index.js
+++ src/cors/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 //process.env.CORS_ORIGIN
 // CORS options
 const corsOptions = {
-  origin: "http://localhost:8001",
+  origin: process.env.CORS_ORIGIN || "http://localhost:8001",
   optionsSuccessStatus: 200
 };
 
