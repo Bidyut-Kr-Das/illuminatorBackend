a9b0bf747b4205b6362b0eed8073be025284d768
Bidyut Kr Das <bkdas2017.bd@gmail.com>
2024-04-06 23:45:21 +0530

    cors change

--- src/cors/index.js
+++ src/cors/index.js
@@ -3,9 +3,10 @@ import cors from "cors";
 //process.env.CORS_ORIGIN
 // CORS options
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || [
+  origin: [
     "http://localhost:8001",
-    "http://localhost:8003"
+    "http://localhost:8003",
+    "https://illuminator-admin.vercel.app"
   ],
   optionsSuccessStatus: 200,
   credentials: true
